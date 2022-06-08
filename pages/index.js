720c5598b339c4a4734ea9200fc139a5792edeed
Katrina Hill <khill365@gmail.com>
2022-06-07 21:38:00 -0700

    replaced globals.css code with tailwinds css code

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react'
 
 export default function Home() {
   // Home is a functional component
   // name the functional component the same as you would a class
+  const [state, setState] = useState({
+    standData: [],
+  });
+
   return (
-    <div>
-      I am alive!
-    </div>
-  )
+    <>
+      <Head>
+        <title>Cookie Stand Admin</>
+      </Head>
+
+    </>
+  );
 }
