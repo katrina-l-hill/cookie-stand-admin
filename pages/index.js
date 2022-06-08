30467f8ab8dff3846b742b89bf6064958f76542c
Katrina Hill <khill365@gmail.com>
2022-06-07 21:23:52 -0700

    deleted code in index.js; revised div tag

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,11 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  // Home is a functional component
+  // name the functional component the same as you would a class
   return (
     <div>
+      I am alive!
     </div>
   )
 }
