a51c97f788fcdda65aed2e05414ab9be40606bfd
Katrina Hill <khill365@gmail.com>
2022-06-09 21:21:25 -0700

    added hooks, contexts, installed dependencies for jsonwebtoken and axios

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+// code attributed to JB Tellez from class demo code
+
+
 import Head from 'next/head'
 // import { useState } from 'react'
 import Header from '../components/Header';
@@ -140,7 +143,9 @@ function CookieStandRow({ info, deleteStand }) {
 
   return (
     <tr>
-      <td>{info.location} <button onClick={clickHandler}>[x]</button></td>
+      <td>{info.location} <button onClick={clickHandler}><svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+      </svg></button></td>
       {info.hourly_sales.map((slot, index) => <td key={index}>{slot}</td>)}
       <td>{info.hourly_sales.reduce((num, sum) => num + sum, 0)}</td>
     </tr>
